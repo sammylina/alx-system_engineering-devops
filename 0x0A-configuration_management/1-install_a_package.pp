079cf2375e6d74cfc3de68a0c2a2667de15a620d
sammyALX <samuelhailu49@gmail.com>
2022-04-27 13:51:18 +0300

    install pip3 if not installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask 2.1.0 using pip3
 
+package {'python3-pip':
+	ensure => installed,
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => pip3,
+  require   => Package['python3-pip'],
 }
