d0fa557916275273810488094b1517493aa628d6
sammyALX <samuelhailu49@gmail.com>
2022-11-02 00:37:31 +0300

    fetch all hot articles of a subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""featch all title of all hot articles for a given subreddit
+"""
+
+import sys
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'user-agent': 'alxafrica'}
+    params = {'after': after}
+
+    res = requests.get(url=url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    data = res.json()
+    listings = data.get('data').get('children')
+    after = data.get('data').get('after')
+    print('after: ', after)
+
+    for article in listings:
+        hot_list.append(article.get('data').get('title'))
+    if after is not None:
+        recurse(subreddit, hot_list=hot_list, after=after)
+
+    return hot_list
