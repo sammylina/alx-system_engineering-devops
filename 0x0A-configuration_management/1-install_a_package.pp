da38d08c9fde339ff0981cbb301df1988dc9e246
sammyALX <samuelhailu49@gmail.com>
2022-11-11 01:29:49 +0300

    modify flask installing script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,9 @@
 # Install flask@2.1.0 from pip3
 
-package { 'pip3 installed':
+package { 'pip3':
     ensure => 'installed',
-    name   => 'pip3'
 }
 
-exec { 'install flask':
-    command => 'pip3 install flask==2.1.0',
-    require => Package['pip3 installed']
+package { 'flask==2.1.0':
+    require => Package['pip3']
 }
