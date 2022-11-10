ce54160269399d8155487d4d4b16d0f091b1f909
sammyALX <samuelhailu49@gmail.com>
2022-11-11 01:42:16 +0300

    rename pip3 package name

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,9 +2,11 @@
 
 package { 'pip3':
     ensure => 'installed',
+    name   => 'python3-pip'
 }
 
-package { 'flask==2.1.0':
+package { 'flask':
     require  => Package['pip3'],
-    provider => 'pip3'
+    provider => 'pip3',
+    name     => 'flask==2.1.0'
 }
