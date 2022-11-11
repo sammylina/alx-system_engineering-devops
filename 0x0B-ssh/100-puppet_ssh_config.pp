c3cdd86ded191d4b8dc2a006ab0c2d000e20dd3f
sammyALX <samuelhailu49@gmail.com>
2022-11-11 11:33:34 +0300

    change ssh client config file location

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # configure ssh client to login without password
 
-$ssh_client = '~/.ssh/config'
+$ssh_client = '/etc/ssh/ssh_config'
 
 file { $ssh_client:
     ensure  => 'file',
     content => 'Host *
-                    PasswordAuthentication no
-                    IdentityFile ~/.ssh/school
-                    PreferredAuthentications publickey',
+        PasswordAuthentication no
+        IdentityFile ~/.ssh/school
+        PreferredAuthentications publickey',
     mode    => '0744',
     group   => 'www-data',
     owner   => 'www-data'
