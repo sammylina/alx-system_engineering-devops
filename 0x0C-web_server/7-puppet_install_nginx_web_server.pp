76196e75658a5a38a1657b292e4086b96470f36b
root <sammyhailu56@gmail.com>
2023-01-19 04:30:05 -0800

    .

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -21,8 +21,7 @@ file_line {'redirect and return 301':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server',
-  line   => '\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  notify => Service['nginx'],
+  line   => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
 }
 
 service {'nginx':
