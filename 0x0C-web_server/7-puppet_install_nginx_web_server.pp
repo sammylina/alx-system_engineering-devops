bc78858c0d76ee88fbb86fd52d43757b443de85e
root <sammyhailu56@gmail.com>
2023-01-19 03:59:03 -0800

    Install and configure ngnix using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,33 @@
+exec {'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+
+#make sure apache is not installed
+package {'apache2.2-common':
+  ensure => absent,
+}
+
+package {'nginx':
+  ensure  => installed,
+  require => [Package['apache2.2-common'],Exec['apt-get update']],
+}
+
+file {'/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World',
+}
+
+file_line {'redirect and return 301':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server',
+  line   => '\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  notify => Service['nginx'],
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
+
