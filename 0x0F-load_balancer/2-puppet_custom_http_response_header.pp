ac435c0960db1023164df2865cad90f87204a04c
root <samuelhailu49@gmail.com>
2023-01-22 12:33:08 -0800

    add custom header using puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+exec {'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+
+#make sure apache is not installed
+package {'apache2.2-common':
+  ensure => absent,
+}
+
+package {'nginx':
+  ensure  => installed,
+  require => [Package['apache2.2-common'],Exec['apt-get update']],
+}
+
+file_line {'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server',
+  line   => "add_header X-Served-By ${host}",
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
+
