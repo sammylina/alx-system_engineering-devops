18de9bcda1dfa9eeab447e81f64d35c7b7b050b0
root <samuelhailu49@gmail.com>
2023-01-22 13:21:35 -0800

    replace double quote with single

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,21 +2,16 @@ exec {'apt-get update':
   command => '/usr/bin/apt-get update',
 }
 
-#make sure apache is not installed
-package {'apache2.2-common':
-  ensure => absent,
-}
-
 package {'nginx':
   ensure  => installed,
-  require => [Package['apache2.2-common'],Exec['apt-get update']],
+  require => Exec['apt-get update'],
 }
 
 file_line {'add custom header':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server',
-  line    => "add_header X-Served-By ${host};",
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 
