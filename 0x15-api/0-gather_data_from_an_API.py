432e201ba235d75019a20ad6514dbbffd5f03df3
sammyALX <samuelhailu49@gmail.com>
2023-02-09 12:06:31 +0300

    fetch todos from REST api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Fetch user's tasks from fake API
+
+if __name__ == '__main__':
+
+    import requests as req
+    import sys
+
+    API_URL = 'https://jsonplaceholder.typicode.com/'
+
+    user_res = req.get(API_URL+'users/{}'.format(sys.argv[1]))
+    todo_req = req.get(API_URL+'users/{}/todos'.format(sys.argv[1]))
+
+    name = user_res.json().get('name')
+    todos = todo_req.json()
+
+    completed_todos = [todo for todo in todos if todo.get('completed') is True]
+
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          len(completed_todos),
+                                                          len(todos)))
+    for todo in completed_todos:
+        print('\t {}'.format(todo.get('title')))
