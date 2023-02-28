220dcebeaecdca4e2f85b8f9b35484fb50325555
SammyAlx <samuelhailu49@gmail.com>
2023-02-28 18:04:31 +0300

    change request endpoint from "top" to "hot"

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,17 +6,17 @@ import requests as req
 
 
 def top_ten(subreddit):
-    """print top 10 reddits
+    """print top 10 hot reddits
     """
 
-    url = 'https://reddit.com/r/{}/top.json'.format(subreddit)
+    url = 'https://reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'alx'}
     params = {'limit': '10'}
 
     res = req.get(url, headers=headers, params=params)
+
     if res.status_code == 200:
         for reddit in res.json().get('data').get('children'):
             print(reddit.get('data').get('title'))
-        return 0
-
-    print(None)
+    else:
+        print(None)
