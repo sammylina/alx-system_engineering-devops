4986acbb44de98f24cd8de5dae133bc5733daf08
SammyAlx <samuelhailu49@gmail.com>
2023-02-28 17:37:00 +0300

    remove __nam__ == "__main__"

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,7 +3,6 @@
 """
 
 import requests as req
-import sys
 
 
 def top_ten(subreddit):
@@ -12,18 +11,12 @@ def top_ten(subreddit):
 
     url = 'https://reddit.com/r/{}/top.json'.format(subreddit)
     headers = {'User-Agent': 'alx'}
-    q_str = {'limit': '10'}
+    params = {'limit': '10'}
 
-    res = req.get(url, headers=headers, params=q_str)
+    res = req.get(url, headers=headers, params=params)
     if res.status_code == 200:
         for reddit in res.json().get('data').get('children'):
             print(reddit.get('data').get('title'))
         return 0
 
     print(None)
-
-
-if __name__ == '__main__':
-
-    subreddit = sys.argv[1] if (len(sys.argv) > 1) else ""
-    top_ten(subreddit)
