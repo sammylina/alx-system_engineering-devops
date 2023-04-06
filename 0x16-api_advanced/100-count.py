10979026367f9990fd8c14c0ed439a16c34ae650
sammyALX <samuelhailu49@gmail.com>
2023-04-06 15:24:21 +0300

    count numbers of key words in subreddit title

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Parse all hot articles title of subreddit and count
+the number of given keywords
+"""
+
+import requests as req
+
+
+def count_words(subreddit, word_list, count_list={}, after=None):
+    """count function
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'alx'}
+    params = {'after': after}
+
+    res = req.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if res.status_code == 200:
+        data = res.json().get('data')
+        after = data.get('after')
+        for reddit in data.get('children'):
+            title = reddit.get('data').get('title')
+            count_list = count(word_list, title, count_list)
+        if after:
+            return count_words(subreddit,
+                               word_list,
+                               count_list=count_list,
+                               after=after)
+        else:
+            print_count_list(count_list)
+            return
+    else:
+        return
+
+
+def print_count_list(count_list):
+    """print count
+    """
+    if len(count_list) != 0:
+        sorted_list = sorted(count_list.items(),
+                             key=lambda item: (item[1], item[0]),
+                             reverse=True)
+        new_list = {}
+        for item in sorted_list:
+            if item[1]:
+                print('{}: {}'.format(item[0], item[1]))
+
+
+def count(word_list, title, count_list):
+    """count
+    """
+    title = title.lower()
+    for word in word_list:
+        if word.lower() in title:
+            if count_list.get(word.lower()):
+                count_list[word.lower()] += title.count(word.lower())
+            else:
+                count_list[word.lower()] = title.count(word.lower())
+    return count_list
